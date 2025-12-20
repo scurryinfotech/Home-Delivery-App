9b4f349ac419cf834aa4fed36c96c75fd6e05e4d
scurry <scurryinfotech@gmail.com>
2025-12-20 11:14:56 +0530

    Remove fire base

--- src/components/auth/AuthContainer.jsx
+++ src/components/auth/AuthContainer.jsx
@@ -20,7 +20,7 @@ const AuthContainer = ({ onAuthSuccess }) => {
     };
    
     const response = await axios.post(
-      'https://grillnshakesapi.scurryinfotechllp.com/api/Order/Login',
+      'https://localhost:7104/api/Order/Login',
       payload
     );
 
@@ -62,7 +62,7 @@ const AuthContainer = ({ onAuthSuccess }) => {
       };
       const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkdyaWxsX05fU2hha2VzIiwibmJmIjoxNzU5MTMyMzY3LCJleHAiOjE3NjY5MDgzNjcsImlhdCI6MTc1OTEzMjM2N30.ko8YPHfApg0uN0k3kUTLcJXpZp-2s-6TiRHpsiab42Q"
       const response = await axios.post(
-        'https://grillnshakesapi.scurryinfotechllp.com/api/Order/AddUser',
+        'https://localhost:7104/api/Order/AddUser',
         payload,
         {
         headers: {
